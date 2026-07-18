07503faffea68be1420a1b8c05ea8eb88205433b
Leandro Flora <leandrosflora@gmail.com>
2026-07-18 01:53:30 -0300

    feat: configure tenant propagation to orchestrator

--- Configuration/OrchestratorOptions.cs
+++ Configuration/OrchestratorOptions.cs
@@ -5,4 +5,5 @@ public class OrchestratorOptions
     public const string SectionName = "Orchestrator";
 
     public string BaseUrl { get; set; } = string.Empty;
+    public string TenantId { get; set; } = "00000000-0000-0000-0000-000000000001";
 }
