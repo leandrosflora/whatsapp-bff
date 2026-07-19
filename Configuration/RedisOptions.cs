1b899f25866609ebc90589420e7bf9cded3f3b11
Leandro Flora <leandrosflora@gmail.com>
2026-07-18 21:25:10 -0300

    configure durable outbound idempotency

---
+++ Configuration/RedisOptions.cs
@@ -0,0 +1,9 @@
+namespace whatsapp_bff.Configuration;
+
+public sealed class RedisOptions
+{
+    public const string SectionName = "Redis";
+    public string ConnectionString { get; init; } = "localhost:6379";
+    public int PendingTtlSeconds { get; init; } = 120;
+    public int CompletedTtlSeconds { get; init; } = 604800;
+}
