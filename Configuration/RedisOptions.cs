9e4697620345970eacfb6099beff985158440fcc
Leandro Flora <leandrosflora@gmail.com>
2026-07-18 21:56:08 -0300

    keep ambiguous outbound reservations fail closed

--- Configuration/RedisOptions.cs
+++ Configuration/RedisOptions.cs
@@ -4,6 +4,6 @@ public sealed class RedisOptions
 {
     public const string SectionName = "Redis";
     public string ConnectionString { get; init; } = "localhost:6379";
-    public int PendingTtlSeconds { get; init; } = 120;
+    public int PendingTtlSeconds { get; init; } = 86400;
     public int CompletedTtlSeconds { get; init; } = 604800;
 }
